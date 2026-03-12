66a6e38ae48a38db7c50b22ebdee3b233adac70c
loiarababichuck <loiara-ababichuck@estudante.rs.gov.br>
2026-03-12 19:33:54 -0300

    Criando nossa primeira api de exemplo

---
+++ exemplo01/src/main/java/br/com/exercicios/string/exemplo01/controller/Exemplo01.java
@@ -0,0 +1,23 @@
+package br.com.exercicios.string.exemplo01.controller;
+
+import br.com.exercicios.string.exemplo01.service.Exemplo01service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/exemplo01")
+public class Exemplo01 {
+    @Autowired
+    Exemplo01service exemplo01service;
+
+    @GetMapping
+    public String getExemplo(){
+        return "GET: Exemplo01";
+    }
+
+    @GetMapping("soma/{a}/{b}")
+    public String soma(@PathVariable int a, @PathVariable int b){
+        return "A soma dos valores informados é " + exemplo01service.soma(a,b);
+
+    }
+}
---
+++ exemplo01/src/main/java/br/com/exercicios/string/exemplo01/service/Exemplo01service.java
@@ -0,0 +1,11 @@
+package br.com.exercicios.string.exemplo01.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Exemplo01service {
+
+    public int soma(int a, int b){
+        return a+b;
+    }
+}
